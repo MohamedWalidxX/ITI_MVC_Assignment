253848dccf049d4a7fb6b331ad7ffea51de05f38
Mohamed Walid <Moh.Walid2002@gmail.com>
2024-08-19 12:59:32 +0300

    INIT COMMIT

---
+++ ITI_MVC_Asssignment/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ITI_MVC_Asssignment.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
